70c4b051ae8a3b23bae4f6f5c95b4e877b5cff8d
ItsMurumba <kevmurumba@gmail.com>
2024-11-07 20:41:13 +0300

    Adds response types

---
+++ src/types/response.ts
@@ -0,0 +1,33 @@
+export interface Response {
+  status: number;
+  body: string;
+  timestamp: string;
+  headers?: HeadersInit;
+}
+
+export interface Request {
+  protocol?: string;
+  host: string;
+  port?: number | string;
+  path?: string;
+  method?: string;
+  headers?: HeadersInit;
+  body?: string;
+  timestamp: string;
+}
+export interface Orchestration {
+  name: string;
+  request: Request;
+  response: Response;
+}
+export interface OpenHimResponseObject {
+  "x-mediator-urn": string;
+  status: string;
+  response: Response;
+  orchestrations: Orchestration[];
+}
+
+export interface ResponseObject {
+  status: number;
+  body: object;
+}
