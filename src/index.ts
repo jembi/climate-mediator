eff2cd2e659bf1ca5da6620b9bd444098654096d
Drizzentic <derrickrono@gmail.com>
2025-03-25 11:15:49 +0300

    Revert "CU-86c2nxwwm - Create Populations Table on Clickhouse From Original C…"

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import logger from './logger';
 import { getMediatorConfig, initializeBuckets, setupMediator } from './openhim/openhim';
 import routes from './routes/index';
 import { MinioBucketsRegistry } from './types/mediatorConfig';
-import { setupClickhouseTables, createHistoricalDiseaseTable, createPopulationTable } from './utils/clickhouse';
+import { setupClickhouseTables } from './utils/clickhouse';
 
 const app = express();
 
@@ -29,8 +29,6 @@ app.listen(getConfig().port, async () => {
     );
   } else {
     logger.debug('Connection to ClickHouse successful');
-    await createHistoricalDiseaseTable();
-    await createPopulationTable();
     await setupClickhouseTables();
   }
 
